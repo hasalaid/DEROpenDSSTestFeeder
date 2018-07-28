30396e8fbb24c818afc7d8ddd8be70b7c6368a27
Hasala Dharmawardena <hasalaid@gmail.com>
2018-07-28 12:32:57 -0400

    The plot cleaned up for units and the axis was renamed to organize the plots

--- Plot_TestBed.m
+++ Plot_TestBed.m
@@ -8,8 +8,8 @@ figure('DefaultAxesFontSize',18)
 subplot(2,3,1)
 %set(gcf,'FontSize',24)
 plot(h, Q_PV844,h,Q_PV890,h,Q_PV860);  % black *
-title('Q  variation from 6am to 6pm');
-ylabel('Q Kvar');
+title('Reactive power');
+ylabel('Reactive power (kvar)');
 xlabel('Time (hour)');
 xlim([6 18]);
 legend('Q844','Q890','Q860');
@@ -17,22 +17,22 @@ legend('Q844','Q890','Q860');
 %figure('DefaultAxesFontSize',18)
 %set(gcf,'FontSize',24)
 subplot(2,3,2)
-plot(h, P_PV844,h,P_PV890,h,P_PV860,h,P_PV890,h,P_L3000,h,P_bat);  % black *
-title('P  variation from 6am to 6pm');
-ylabel('P kW');
+plot(h, P_PV844,h,P_PV890,h,P_PV860,h,P_PV890,h,P_bat);  % black *
+title('Active power');
+ylabel('Active power (kW)');
 xlabel('Time (hour)');
 xlim([6 18]);
-legend('P844','P890','P860','S890','L3000','Pbat');
+legend('P844','P890','P860','P890','Pbat');
 
 %figure('DefaultAxesFontSize',18)
 %set(gcf,'FontSize',24)
 subplot(2,3,3)
 plot(h, TotalActivePower,h, TotalReactivePower);  % black *
-title('Feeder total power flow ');
-ylabel('Power in kVA ');
+title('Feeder total power flow');
+ylabel('Apparent power (kVA) ');
 xlabel('Time (hour)');
 xlim([6 18]);
-legend('P Total kW','Q Total Kvar');
+legend('P (kW)','Q (kvar)');
 
 
 
@@ -40,17 +40,21 @@ legend('P Total kW','Q Total Kvar');
 %set(gcf,'FontSize',24)
 subplot(2,3,4)
 plot(h, V844,h,V890,h,V860,h,V812,h,V828,h,V800);  % black *
-title('V  variation from 6am to 6pm');
-ylabel('V pu');
-xlabel('Time (hour)');
+title('Bus voltage');
+ylabel('Voltage (pu)');
+xlabel('Time (hour)');;
 xlim([6 18]);
 legend('V844','V890','V860','V812','V828','V800');
 
 %figure('DefaultAxesFontSize',18)
 subplot(2,3,5)
 plot(h, Regtap1,h,Regtap2);
-title('Tap Changer state');
-legend('Tap Changer 1 phase a','Tap Changer 2 phase a')
+title('Tap changer (TC) state');
+xlabel('Time (hour)');
+ylabel('Tap position');
+ylim([-4 17]);
+xlim([6 18]);
+legend('TC 1 phase a','TC 2 phase a')
 str = sprintf('Reg1A # of tap Ops: %d and Reg2A # of tap Ops: %d. MSE of voltage is %d. Total energy loss is %d MWh', CountTaps(Regtap1),CountTaps(Regtap2),err,energyloss);
 dim = [.2 .5 .3 .5];
 
@@ -58,8 +62,8 @@ annotation('textbox',dim,'String',str,'FitBoxToText','on');
 
 subplot(2,3,6)
 plot(h, TotalLoss);  % black *
-title('Losses');
-ylabel('Total loss in  kW');
+title('Total losses');
+ylabel('Power loss (kW)')
 xlabel('Time (hour)');
 xlim([6 18]);
 legend('Total Loss');
@@ -67,8 +71,8 @@ legend('Total Loss');
 
 figure('DefaultAxesFontSize',18)
 plot(h, batkWhstored);  % black *
-title('Current SOC');
-ylabel('SOC in kWh');
+title('State of charge');
+ylabel('SOC (kWh)');
 xlabel('Time (hour)');
 xlim([6 18]);
 legend('SOC of Battery');
